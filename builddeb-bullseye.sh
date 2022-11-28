a71118709aac1a6ad1dcb97a85e1be3ba0fb3a0b
Philipp Rosenberger <p.rosenberger@kunbus.com>
2022-11-28 09:26:05 +0100

    Move the Dockerfile to a $FLAVOR dir
    With moving the Docker file to the $FLAVOR directory we can provide all
    flavors in one branch. This should make it easier to maintain all the
    different flavors. Also the useability should get better.
    
    Signed-off-by: Philipp Rosenberger <p.rosenberger@kunbus.com>

--- builddeb-bullseye.sh
+++ builddeb-bullseye.sh
@@ -4,8 +4,8 @@ set -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-export FLAVOUR="bullseye"
+export FLAVOR="bullseye"
 
-( cd "$DIR" && docker build -t "builddeb-$FLAVOUR" . )
+( cd "${DIR}/${FLAVOR}" && docker build -t "builddeb-$FLAVOR" . )
 
 exec "$DIR/builddeb.sh" "$@"
