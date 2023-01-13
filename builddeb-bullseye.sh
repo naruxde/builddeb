fc18e2ecaf28b16ddffd9ba284f7fb6f9861f30f
Philipp Rosenberger <p.rosenberger@kunbus.com>
2023-01-13 16:33:46 +0100

    builddep-${FLAVOR}.sh: Update the conatinaer build command
    * pull always the newest base image
    * specify the Dockerfile with the -f option
    
    Signed-off-by: Philipp Rosenberger <p.rosenberger@kunbus.com>

--- builddeb-bullseye.sh
+++ builddeb-bullseye.sh
@@ -6,6 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export FLAVOR="bullseye"
 
-( cd "${DIR}/${FLAVOR}" && docker build -t "builddeb-$FLAVOR" . )
+( cd "${DIR}" && docker build -t "builddeb-${FLAVOR}" --pull -f "${FLAVOR}/Dockerfile" . )
 
 exec "$DIR/builddeb.sh" "$@"
