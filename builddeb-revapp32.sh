a7863354c46a42053506f21f3df70934338f097a
Sven Sager <akira@narux.de>
2023-10-26 18:45:02 +0200

    feat: Add RevApp Python container buster-based
    These containers build the RevApps for the Revolution Pi with Python
    3.11. The smallest supported release is Debian-Buster. All newer
    releases are automatically supported.
    
    Signed-off-by: Sven Sager <akira@narux.de>

---
+++ builddeb-revapp32.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export FLAVOR="revapp32"
+
+( cd "${DIR}" && docker build -t "builddeb-${FLAVOR}" --pull -f "${FLAVOR}/Dockerfile" . )
+
+exec "$DIR/builddeb.sh" "$@"
