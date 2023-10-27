a73c1a0403e2c50801378d75c379591b6480e624
Sven Sager <akira@narux.de>
2023-10-27 09:33:00 +0200

    feat: Do not create a mirrored user on macOS
    The mirrored user is important because the generated files from the
    container have the owner 'root'. This is impractical if the executing
    user does not have root rights. On a macOS system, ownership is
    transferred to the active macOS user and the files can be used
    directly.
    
    Signed-off-by: Sven Sager <akira@narux.de>

--- run.sh
+++ run.sh
@@ -24,10 +24,18 @@ cd /tmp/deps
 mk-build-deps --install --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' "$WORK/$PACKAGE/debian/control"
 cd "$WORK/$PACKAGE"
 
-# create a user with the same name and gid as the calling user
-groupadd -g "$BUILD_GID" "$BUILD_GNAME"
-useradd -l -m -s /bin/bash -u "$BUILD_UID" -g "$BUILD_GNAME" "$BUILD_UNAME"
-cp -r /root/gnupg "/home/${BUILD_UNAME}/.gnupg"
-chown -R "${BUILD_UNAME}:${BUILD_GNAME}" "/home/${BUILD_UNAME}/.gnupg"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    cp -r /root/gnupg /root/.gnupg
+    
+    # On macOS we can just start the build and get the files with macOS owner
+    ${BUILD_CMD}
+else
+    # On 'other' OSes create a user with the same name and gid as the calling user
+    groupadd -g "$BUILD_GID" "$BUILD_GNAME"
+    useradd -l -m -s /bin/bash -u "$BUILD_UID" -g "$BUILD_GNAME" "$BUILD_UNAME"
+    cp -r /root/gnupg "/home/${BUILD_UNAME}/.gnupg"
+    chown -R "${BUILD_UNAME}:${BUILD_GNAME}" "/home/${BUILD_UNAME}/.gnupg"
 
-su -c "${BUILD_CMD}" "${BUILD_UNAME}"
+    # And start the package build as the created user
+    su -c "${BUILD_CMD}" "${BUILD_UNAME}"
+fi
